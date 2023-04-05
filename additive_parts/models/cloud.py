751961827bca6f1485c3908d90e1b4c8b16cb41a
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-04-04 22:01:50 -0700

    project struture update, point cloud work

---
+++ additive_parts/models/cloud.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import pdb
+
+
+class PointCloudProcessor(nn.Module):
+    def __init__(
+        self,
+        input_features,
+        nhead,
+        nlayer,
+        dim_feedforward,
+        batch_first,
+        linear_layers,
+        device=None,
+    ):
+        super().__init__()
+        self.nhead = nhead
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            input_features * nhead,
+            nhead,
+            dim_feedforward=dim_feedforward,
+            device=device,
+            batch_first=batch_first,
+        )
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, nhead)
+        self.post_process = nn.Sequential()
+        for i, l in enumerate(linear_layers):
+            if i == 0:
+                self.post_process.append(nn.LazyLinear(l))
+                # self.post_process.append(nn.ReLU())
+            elif i == len(linear_layers) - 1:
+                self.post_process.append(nn.Linear(linear_layers[i - 1], 1))
+            else:
+                self.post_process.append(nn.Linear(linear_layers[i - 1], l))
+                self.post_process.append(nn.ReLU())
+
+    def forward(self, tensors, src_key_padding=None):
+        out = self.encoder_layer(tensors, src_key_padding_mask=src_key_padding)
+        # out = self.encoder(
+        #     tensors.repeat(1, 1, self.nhead), src_key_padding_mask=src_key_padding
+        # )
+        stacked = torch.concat(
+            [
+                torch.min(out, dim=1)[0],
+                torch.mean(out, dim=1),
+                torch.max(out, dim=1)[0],
+            ],
+            dim=1,
+        )
+        return self.post_process(stacked)
---
+++ dim_check.py
@@ -0,0 +1,21 @@
+from additive_parts.models.cloud import PointCloudProcessor
+import pdb
+import torch
+import torch.nn as nn
+
+torch.manual_seed(0)
+p = PointCloudProcessor(3, 8, 1, 128, True, [16, 16])
+rand = torch.rand((10, 10, 24), requires_grad=True)
+out = p(rand)
+print(out.shape)
+loss = nn.MSELoss()(out, torch.zeros_like(out))
+print(loss)
+
+state_dict = p.state_dict()
+# state_dict = p.post_process.state_dict()
+# print(state_dict.keys())
+loss.backward()
+print(p.encoder.layers[0].self_attn.in_proj_weight.grad.view(-1)[:5])
+# print(p.encoder_layer.self_attn.in_proj_bias.grad.view(-1)[:5])
+print(p.post_process[0].weight.grad.view(-1)[:5])
+print(rand.grad.view(-1)[:5])
