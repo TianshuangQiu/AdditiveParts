1359db4690758e6b811a190d53c0e29c33f0c455
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-04-11 21:50:45 -0700

    first work on tensormaker

---
+++ additive_parts/utils/tensormaker.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+from jsonmaker import JsonMaker
+
+parser = argparse.ArgumentParser()
+parser.add_argument("csv_dir", help="csv base folder")
+parser.add_argument("json_dir", help="json output path")
+parser.add_argument("base_dir", help="base directory")
+parser.add_argument("cloud_tensor", help="path to store tensor for point clouds")
+parser.add_argument("norms_tensor", help="path to store tensor fo norms")
+args = parser.parse_args()
+
+j = JsonMaker(args.base_dir, args.csv_dir)
+vox_256, stl = j.make_json(args.json_dir)
