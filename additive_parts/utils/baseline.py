48433577a60ab25541edfc82fa45f05ecc1c55d9
Y-A-Song <yifansong@berkeley.edu>
2023-04-19 16:57:39 -0700

    running baseline_1

--- additive_parts/utils/baseline.py
+++ additive_parts/utils/baseline.py
@@ -1,2 +1,18 @@
+import sys
+sys.path.insert(0, '/global/scratch/users/yifansong/stuff/lib/python3.9/site-packages')
+import numpy as np
 import trimesh
+import func_timeout
 
+# returns the F norm of the difference between the (most likely) stable pose and the identity matrix
+def trimesh_score_src(filepath):
+    try:
+        tmesh = trimesh.load_mesh(filepath, file_type = 'stl')
+        dict =  trimesh.poses.compute_stable_poses(tmesh, sigma=0, n_samples=1, threshold = 0.0)
+        pose = dict[0][0]
+        identity = np.identity(4)
+        score = np.linalg.norm(np.subtract(identity, pose))
+        return score
+    except:
+	pass
+	return -98
---
+++ run_baseline.py
@@ -0,0 +1,28 @@
+from additive_parts.utils.baseline import trimesh_score
+import json
+import numpy as np
+from tqdm import tqdm
+
+with open('/global/scratch/users/ethantqiu/Data/stl.json', 'r') as infile:
+    json_object = json.load(infile)
+
+out = '/global/scratch/users/yifansong/stl_works0_stablepose.json'
+outfile = open(out, 'w')
+
+keys = list(json_object.keys())
+
+keys_array = np.array(keys)
+#print('array done')
+keys_split = np.array_split(keys_array, 10)
+#print('split done')
+for k_list in tqdm(keys_split):
+    result = {k: trimesh_score(k) for k in tqdm(k_list)}
+    json.dump(result, outfile)
+
+infile.close()
+outfile.close()
+#result = {key: trimesh_score(key) for key,v in json_object.items()}
+
+#out = '/global/scratch/users/yifansong/stl_v1_stablepose.json'
+#with open(out, 'w') as outfile:
+#    json.dump(result, outfile)
