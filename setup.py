3e099dfd800546a4c3e336a2091b717ead3385fe
Tianshuang(Ethan) Qiu <ethantqiu@berkeley.edu>
2023-04-19 21:29:33 -0700

    initial trianing script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name="AdditiveParts",
+    version="1.0.0",
+    author="ethantqiu",
+    license="MIT",
+    packages=find_packages(),
+)
