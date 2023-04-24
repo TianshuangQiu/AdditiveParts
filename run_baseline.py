9c04109cf70c32eb21ae6ff407205be986d92bec
Y-A-Song <yifansong@berkeley.edu>
2023-04-24 13:08:24 -0700

    3*3baseline

--- run_baseline.py
+++ run_baseline.py
@@ -12,14 +12,17 @@ outfile = open(out, "w")
 
 keys = list(json_object.keys())
 
-keys_array = np.array(keys)
+#keys_array = np.array(keys)
 # print('array done')
-keys_split = np.array_split(keys_array, 10)
+#keys_split = np.array_split(keys_array, 10)
 # print('split done')
-for k_list in tqdm(keys_split):
-    result = {k: trimesh_score(k) for k in tqdm(k_list)}
-    json.dump(result, outfile)
-result = process_map(trimesh_score, k_list, chunksize=31250)
+#for k_list in tqdm(keys_split):
+    #result = {k: trimesh_score(k) for k in tqdm(k_list)}
+    #json.dump(result, outfile)
+result = process_map(trimesh_score, keys, chunksize=31250)
+#result = process_map(trimesh_score, keys, chunksize=20)
+resultDict = dict(zip(keys, result))
+json.dump(resultDict, outfile)
 
 infile.close()
 outfile.close()
