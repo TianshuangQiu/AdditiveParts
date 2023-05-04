6f8024240a1454f62fe547739857ea2eb7514e58
TianshuangQiu <ethantqiu@berkeley.edu>
2023-05-04 15:36:36 -0700

    rename cloud model

--- README.md
+++ README.md
@@ -4,19 +4,25 @@ UC Berkeley EECS282 Project
 ## Overview
 This project has 4 components, each finished by a different team member. The parts are: baseline, 3D CNN, Mesh CNN, and pointcloud encoder. For the baseline and pointcloud computations, the instructions are outlined in this document. For 3D CNN and Mesh CNN, their respective README's are in their folders.
 
+## File Structure
+Please download the files from this folder: https://drive.google.com/drive/folders/1jCzhg4bwJk7lqaEBTcfoWZch0BIdxavM?usp=sharing and arrange into the following structure
+```
+BASE_DIR
+├── AdditiveParts
+├── rawcloud
+├── rawcloud.json
+├── rawnorm
+├── rawnorm.json
+├── repaired_files
+```
+
+
 ## Baseline
-### To Install Original Printability Scores:
-1. Download the .json file from the following link:https://drive.google.com/drive/folders/1ZDbtCF1YcgrH9zyQXbL-eH95N4o1lIvK?usp=sharing
-### To Run
 1. Change filepath at line 7 to filepath of downloaded .json file
 2. Change filepath at line 10 to target filepath of result file
 3. ```python run_baseline.py```
 
 ## Point Cloud Encoder
-### Downloading the Data:
-Meshes: https://drive.google.com/drive/folders/1C0MGixYalkqlBkXeAsyGjXVHUfst113t?usp=share_link
-Labels: https://drive.google.com/drive/folders/1XQ1MZiSwdev-85kfswiY5qveu_y-cWvt?usp=sharing
-### Running the Network
 1. Unzip the file and clone the repositories
 2. The file structure should be `/base/rotated_files`, `/base/tweaker_score`, and `base/AdditiveParts`
 3. Run utils/tensormaker.py specifying the path to your base folder, the rotated_files folder, and the folder for CSVs. This script will go through the meshes and sample points and extract norms
--- additive_parts/utils/tensormaker.py
+++ additive_parts/utils/tensormaker.py
@@ -22,23 +22,22 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-j = JsonMaker(args.base_dir, args.csv_dir)
-vox_256, stl, vox_64 = j.make_json(args.json_dir)
+# j = JsonMaker(args.base_dir, args.csv_dir)
+# vox_256, stl, vox_64 = j.make_json(args.json_dir)
 
-args.json_dir = os.path.join(args.json_dir, "stl.json")
+args.json_dir = os.path.join(args.json_dir, "sanitized_dict.json")
 
 
 def make_tensor(stl_path):
     global max_face
     name = stl_path.split("/")[-1]
-    # cloud = point_cloudify(stl_path, 4096)
+    cloud = point_cloudify(stl_path, 4096)
     norms = seven_dim_extraction(stl_path)
-    # torch.save(torch.from_numpy(cloud), os.path.join(cloud_path, name))
-    # torch.save(
-    #     (torch.from_numpy(norms[0]), torch.from_numpy(norms[1])),
-    #     os.path.join(norm_path, name),
-    # )
-    cloud_path = ""
+    torch.save(torch.from_numpy(cloud), os.path.join(cloud_path, name))
+    torch.save(
+        (torch.from_numpy(norms[0]), torch.from_numpy(norms[1])),
+        os.path.join(norm_path, name),
+    )
     return (
         {os.path.join(cloud_path, name): d[stl_path]},
         {os.path.join(norm_path, name): d[stl_path]},
@@ -48,12 +47,12 @@ def make_tensor(stl_path):
 with open(args.json_dir, "r") as r:
     d = json.load(r)
 
-# cloud_path = os.path.join(args.base_dir, "rawcloud")
-norm_path = os.path.join(args.base_dir, "norm")
-# os.makedirs(cloud_path, exist_ok=True)
+cloud_path = os.path.join(args.base_dir, "rawcloud")
+norm_path = os.path.join(args.base_dir, "rawnorm")
+os.makedirs(cloud_path, exist_ok=True)
 os.makedirs(norm_path, exist_ok=True)
 
-out_paths = process_map(make_tensor, d.keys(), chunksize=3000)
+out_paths = process_map(make_tensor, d.keys(), chunksize=500)
 # for k in d.keys():
 #     make_tensor(k)
 cloud_dict = {}
@@ -62,7 +61,7 @@ for o in out_paths:
     cloud_dict.update(o[0])
     norms_dict.update(o[1])
 
-# with open(os.path.join(args.base_dir, "rawcloud.json"), "w") as w:
-#     json.dump(cloud_dict, w)
-with open(os.path.join(args.base_dir, "norm.json"), "w") as w:
+with open(os.path.join(args.base_dir, "rawcloud.json"), "w") as w:
+    json.dump(cloud_dict, w)
+with open(os.path.join(args.base_dir, "rawnorm.json"), "w") as w:
     json.dump(norms_dict, w)
