fc035fd05f24fb2a8a9d4467df5b1c543730d74a
kommander-keene <kboondich@gmail.com>
2023-05-04 15:15:40 -0700

    wefio

--- meshcnn/scripts/process.sh
+++ meshcnn/scripts/process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 search_and_process() {
-   find ../data -name "*.stl" -type f | head -n 5000 | xargs -I {} sh -c '
-   /Applications/Blender.app/Contents/MacOS/Blender --background --python stl_to_obj.py -- "$0"
-   ' {}
+   # find ../std_files -name "*.stl" -type f | head -n 5000 | xargs -I {} sh -c '
+   # /Applications/Blender.app/Contents/MacOS/Blender --background --python stl_to_obj.py -- "$0"
+   # ' {}
    
    find ./output -name "*.obj" -type f | head -n 5000 | xargs -I {} sh -c '
    /Applications/Blender.app/Contents/MacOS/Blender --background --python normalize.py "$0" 512 "$0"
