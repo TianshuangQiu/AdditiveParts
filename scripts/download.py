658fa3ea3b57f8b8000509c4ffb9af6ba91ed0ee
ethantqiu <ethantqiu>
2023-12-19 21:32:00 +0000

    added util script to download data

---
+++ scripts/download.py
@@ -0,0 +1,13 @@
+import gdown
+import json
+import shutil
+import os
+
+os.makedirs("data", exist_ok=True)
+
+with open("config/data_path.json", "r") as r:
+    drive_dict = json.load(r)
+
+for key, value in drive_dict.items():
+    gdown.download(url=value, output="data/" + key, fuzzy=True)
+    shutil.unpack_archive("data/" + key, f"data/{key}")
