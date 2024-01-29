3a4daf6096ab327570bcdac8b401570050b7389d
ethantqiu <ethantqiu>
2024-01-29 22:44:28 +0000

    split change

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import pdb
 from matplotlib import pyplot as plt
 
+from scipy.stats import percentileofscore as PoS
+
 random.seed(69420)
 np.random.seed(69420)
 
@@ -19,37 +21,36 @@ rest_of_data = []
 labels = []
 for i in tqdm(items):
     labels.append(float(i[1]))
-    if len(bench_mark) < 5000:
-        if float(i[1]) <= avg:
-            bench_mark.append(i)
-        else:
-            rest_of_data.append(i)
-    elif len(bench_mark) < 10000:
-        if float(i[1]) > avg:
-            bench_mark.append(i)
-        else:
-            rest_of_data.append(i)
-    else:
-        rest_of_data.append(i)
 labels = np.array(labels)
-pdb.set_trace()
-# with open("data/benchmark.json", "w") as w:
-#     json.dump({i: j for i, j in bench_mark}, w)
+for i in tqdm(items):
+    if len(bench_mark) < 10000:
+        bench_mark.append((i[0], PoS(labels, float(i[1]), "weak")))
+    else:
+        rest_of_data.append((i[0], PoS(labels, float(i[1]), "weak")))
+    # else:
+    #     rest_of_data.append(i)
+plt.clf()
+plt.hist(labels)
+plt.title(f"{len(labels)} elements")
+plt.savefig(f"benchmark.png")
 
-# with open("data/agg.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data}, w)
+with open("data/benchmark.json", "w") as w:
+    json.dump({i: j for i, j in bench_mark}, w)
 
-# with open("data/test.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data[:10000]}, w)
+with open("data/agg.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data}, w)
 
-# with open("data/10000.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data[10000:20000]}, w)
+with open("data/test.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data[:10000]}, w)
 
-# with open("data/50000.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data[10000:60000]}, w)
+with open("data/10000.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data[10000:20000]}, w)
 
-# with open("data/100000.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data[10000:110000]}, w)
+with open("data/50000.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data[10000:60000]}, w)
 
-# with open("data/200000.json", "w") as w:
-#     json.dump({i: j for i, j in rest_of_data[10000:210000]}, w)
+with open("data/100000.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data[10000:110000]}, w)
+
+with open("data/200000.json", "w") as w:
+    json.dump({i: j for i, j in rest_of_data[10000:210000]}, w)
